823d028d346c56fc4821aa6118e40ed8eef65afa
Valerio Barrila <valerio.barrila@gmail.com>
2014-06-03 16:14:18 +0200

    added test coverage with istanbul #19

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     mocha = require('gulp-mocha'),
     docco = require('gulp-docco'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    istanbul = require('gulp-istanbul');
 
 // Help module
 require('gulp-help')(gulp);
@@ -13,6 +14,18 @@ gulp.task('test', 'Run the application tests', function () {
         .pipe(mocha({
             reporter: 'spec'
         }));
+});
+
+gulp.task('coverage', 'Create istanbul code coverage report form tests', function (cb) {
+    gulp.src(['lib/**/*.js', 'index.js'])
+        .pipe(istanbul())
+        .on('finish', function () {
+            var must = require('must');
+            gulp.src(['./examples/**/*.test.js', './tests/**/*.test.js'])
+                .pipe(mocha())
+                .pipe(istanbul.writeReports())
+                .on('end', cb);
+        });
 });
 
 gulp.task('docs', 'Build the documentation', function () {
