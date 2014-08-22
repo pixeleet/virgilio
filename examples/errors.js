919e3bc3eeaae7737fd868674392239a50dbad84
Jasper Woudenberg <jasper.woudenberg@icemobile.com>
2014-08-22 21:11:15 +0200

    Added example-test for errors.

---
+++ examples/errors.js
@@ -0,0 +1,22 @@
+var Virgilio = require('../');
+var virgilio = new Virgilio();
+
+//Defining a simple error.
+virgilio.registerError$({
+    name: 'FooError'
+});
+var fooError = new virgilio.FooError('foo!');
+console.log(fooError.name);     //=> 'FooError'
+console.log(fooError.message);  //=> 'foo!'
+
+//Defining an error with a custom constructor.
+virgilio.registerError$({
+    name: 'DivideByZeroError',
+    init: function(number) {
+        this.message = 'Can`t divide ' + number + ' by zero.';
+        this.failingNumber = number;
+    }
+});
+var divideByZeroError = new virgilio.DivideByZeroError(5);
+console.log(divideByZeroError.message);         //=> 'Can`t divide 5 by zero.'
+console.log(divideByZeroError.failingNumber);   //=> 5
