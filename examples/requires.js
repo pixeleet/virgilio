e38b1188d3d7e148991729c912ac0ba61d89cd85
Luca Maraschi <luca.maraschi@gmail.com>
2014-10-27 09:44:11 +0100

    Fixed tests

--- examples/requires.js
+++ examples/requires.js
@@ -2,20 +2,24 @@ var Virgilio = require('../');
 var virgilio = new Virgilio();
 var assert = require('assert');
 
+// Testing the require with explicit name
 virgilio.shareRequire$('foo', module.exports.foo = function foo() {
     return 'foo';
 });
+var test1 = virgilio.require$.foo();
+console.log(test1); //=> 'foo'
 
+// Testing the require with implicit name
 virgilio.shareRequire$(module.exports.asd = function asd() {
     return 'asd';
 });
+var test2 = virgilio.require$.asd();
+console.log(test2); //=> 'asd'
 
+// testing the require override
 virgilio.shareRequire$(module.exports.asd = function asd() {
     return 'asd2';
 });
+var test3 = virgilio.require$.asd();
+console.log(test3); //=> 'asd'
 
-var test1 = virgilio.require$.foo();
-var test2 = virgilio.require$.asd();
-
-console.log(test1);
-console.log(test2);
